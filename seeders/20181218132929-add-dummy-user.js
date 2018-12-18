4bf69b432e89c056fb5dd7e0cc7e50b58f956f3b
ShadowM8 <anton2014lenovo@gmail.com>
2018-12-18 20:50:49 +0700

    add seed dummy user

--- seeders/20181218132929-add-dummy-user.js
+++ seeders/20181218132929-add-dummy-user.js
@@ -12,6 +12,74 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
+    return queryInterface.bulkInsert('Users', [
+      {
+        username: 'Joko Tingkir',
+        password: '12345',
+        email: 'joko@mail.com',
+        reputasi: 1.5
+      },
+      {
+        username: 'Pak Kumis',
+        password: '54321',
+        email: 'kumis@mail.com',
+        reputasi: 7.5
+      },
+      {
+        username: 'Tony Hawk',
+        password: '54213',
+        email: 'tony@mail.com',
+        reputasi: 3.5
+      },
+      {
+        username: 'Solid Snake',
+        password: '33333',
+        email: 'solid@mail.com',
+        reputasi: 5.5
+      },
+      {
+        username: 'Hwoarang',
+        password: '33333',
+        email: 'solid@mail.com',
+        reputasi: 7.5
+      },
+      {
+        username: 'Seo Hyun',
+        password: '33333',
+        email: 'solid@mail.com',
+        reputasi: 8.5
+      },
+      {
+        username: 'Ntona Sobinowi',
+        password: '11111',
+        email: 'ntona@mail.com',
+        reputasi: 3.5
+      },
+      {
+        username: 'Lionel Messi',
+        password: '77777',
+        email: 'lionel@mail.com',
+        reputasi: 5.6
+      },
+      {
+        username: 'Timon',
+        password: '11333',
+        email: 'timon@mail.com',
+        reputasi: 4.5
+      },
+      {
+        username: 'Alberto Mamamia',
+        password: '78911',
+        email: 'alberto@mail.com',
+        reputasi: 5.9
+      },
+      {
+        username: 'Pumba',
+        password: '78199',
+        email: 'pumba@mail.com',
+        reputasi: 6.9
+      }
+    ])
   },
 
   down: (queryInterface, Sequelize) => {
@@ -22,5 +90,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
+   return queryInterface.bulkDelete('Users', null, {})
   }
 };
