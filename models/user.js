d69ad7af906e1d4ef4812245724988e4db774ab0
ChristianHS23 <christian.sihotang23@gmail.com>
2018-12-18 22:00:20 +0700

    Password validate in user model

--- models/user.js
+++ models/user.js
@@ -2,7 +2,14 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: DataTypes.STRING,
-    password: DataTypes.STRING,
+    password: {type: DataTypes.STRING,
+      validate: {
+        is : {
+          args : /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/i ,
+          msg : "Password Must Have 1 Uppercase 1 and 8 characters"
+        }
+      }
+    },
     email: DataTypes.STRING,
     reputasi: DataTypes.REAL
   }, {});
