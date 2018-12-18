caf179ef32687b47f4baac0eca363640299d8814
ShadowM8 <anton2014lenovo@gmail.com>
2018-12-18 21:59:49 +0700

    edit models/user.js

--- models/user.js
+++ models/user.js
@@ -1,9 +1,58 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    username: DataTypes.STRING,
+    username: {
+      type : DataTypes.STRING,
+      validate : {        
+        isUnique : function(value){          
+          return User.findOne({
+            where : {
+              username:value,
+              id : {
+                [Op.ne] : this.id
+              }
+            }            
+          })
+          .then((data)=>{
+            if(data){
+              throw `username already registered`
+            }            
+          })
+          .catch(err=>{
+            throw new Error(err)
+          })
+        },
+        min : 5
+      }
+    },
     password: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type : DataTypes.STRING,
+      validate : {        
+        isUnique : function(value){          
+          return User.findOne({
+            where : {
+              email:value,
+              id : {
+                [Op.ne] : this.id
+              }
+            }            
+          })
+          .then((data)=>{
+            if(data){
+              throw `email already registered`
+            }            
+          })
+          .catch(err=>{
+            throw new Error(err)
+          })
+        },
+        isEmail : {
+          args:true,
+          msg: `Email format is wrong!`
+        }
+      }
+    },
     reputasi: DataTypes.REAL
   }, {});
   User.associate = function(models) {
