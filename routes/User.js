9a0ed053021fdfbfea793430c6b0ebcfdbd0fbc8
ChristianHS23 <christian.sihotang23@gmail.com>
2018-12-19 13:06:06 +0700

    edit user get and post register

--- routes/User.js
+++ routes/User.js
@@ -2,15 +2,28 @@ const express = require('express')
 const router = express.Router()
 const Model = require('../models')
 
-router.get('/register',(req,res)=>{
+router.post('/register',(req,res)=> {
     // res.send('ini tampilan list user aktif dari forum kami')
+    let info = `Success to register`
     let objUser = {
         username : req.body.username,
         password : req.body.password,
         email : req.body.email,
-        
     }
-    Model.User.create()
+
+    Model.User.create(objUser)
+    .then(()=> {
+        res.redirect(`/user/register?info=${info}`)
+    })
+    .catch((err)=> {
+        res.redirect(`/user/register?err=${err}`)
+    })
+})
+
+router.get(`/register`, (req, res)=> {
+    let info = req.query.info
+    let err = req.query.err
+    res.render('')
 })
 
 router.get('/edit/:id',(req,res)=>{
