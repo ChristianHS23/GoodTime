a720f8dbd13ec04729a70635e69bf4ee997ea442
ShadowM8 <anton2014lenovo@gmail.com>
2018-12-19 11:41:02 +0700

    add views

--- routes/Index.js
+++ routes/Index.js
@@ -2,8 +2,8 @@ const router = require('express').Router()
 const Model = require('../models')
 
 
-router.get('/', (req, res)=> {
-    res.send(`ini Page Home `)
+router.get('/', (req, res)=> {    
+    res.render('index.ejs')
 })
 
 module.exports = router
