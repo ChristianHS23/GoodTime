61f56643fe88178c05d266115d967d737cbedb0f
ChristianHS23 <christian.sihotang23@gmail.com>
2018-12-21 09:31:37 +0700

    change port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 const Post = require('./routes/Post')
 const Role = require('./routes/Role')
 const Tag = require('./routes/Tag')
